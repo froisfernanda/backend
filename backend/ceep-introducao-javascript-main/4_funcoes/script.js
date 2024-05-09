18fef33acad3a82a575dcbeab2853fa3f4c791dd
froisfernanda <127303960+froisfernanda@users.noreply.github.com>
2024-05-09 17:28:57 -0300

    Add files via upload

---
+++ backend/ceep-introducao-javascript-main/4_funcoes/script.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+let nome ="joao";
+let idade = 18;
+let anoAtual = 2024;
+
+let anoNacimento = anoAtual - idade;
+console.log(` O ${nome} nasceu em ${anoNacimento} !!`);
+
+let nome1 = "Mauricio";
+anoNacimento1 = 2024 - 15;
+let anoNacimento1 = anoAtual - idade;
+console.log(` O ${nome1} nasceu em ${anoNacimento} !!`);
+
+let nome2 ="Carlos";
+let noNacimento = 2024 - 12;
+let anoNacimento2 = anoAtual - idade;
+console.log(` O ${nome2} nasceu em ${anoNacimento} !!`);
